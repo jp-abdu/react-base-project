5b1b80b45a65e924ad541b4c6957060b69d98443
jp-abdu <joaopedroabdu@gmail.com>
2024-08-22 13:39:06 -0300

    atletas

--- src/services/AtletasBotafogo.js
+++ src/services/AtletasBotafogo.js
@@ -1,6 +1,6 @@
 import { handleError, handleResponse } from "./ServiceUtils";
 
-const url = "https://botafogo-atletas.mange.li/2024-1p"
+const url = "https://botafogo-atletas.mange.li/2024-1"
 
 const getAll = async () => {
   const response = await (fetch(`${url}/all`).catch(handleError));
