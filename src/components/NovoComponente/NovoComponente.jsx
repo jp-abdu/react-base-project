1aee373f8bd43ba508d66cf7ed7e693447cdf053
JOAO PEDRO MARTINS ABDU <202401000981@alunos.ibmec.edu.br>
2024-10-03 17:04:15 -0300

    mudanças na branch

---
+++ src/components/NovoComponente/NovoComponente.jsx
@@ -0,0 +1,8 @@
+const NovoComponente = () => (
+    <div>
+        <h2>conteudo do componente</h2>
+        <p>Um paragrafo</p>
+    </div>
+);
+
+export default NovoComponente;
---
+++ src/pages/Nova.jsx
@@ -0,0 +1,11 @@
+import NovoComponente from "../components/NovoComponente/NovoComponente";
+
+const Nova = () => {
+    return (<>
+        <h2>Titulo h1</h2>
+        <p>Ola</p> 
+        <NovoComponente /> 
+        </>);
+}
+
+export default Nova;
